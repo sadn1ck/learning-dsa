a8e3c3d59c69ae301ebac64dc48e0d42f983a87e
Anik Das <anikdas0811@gmail.com>
2020-10-22 18:27:34 +0530

    STFOOD done

---
+++ codechef/LRNDSA02/STFOOD.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+
+#define ff first
+#define ss second
+#define pb push_back
+#define pf push_front
+
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define ins insert
+#define mp make_pair
+#define len(s) s.length()
+#define on cout << "\n";
+#define o2(a, b) cout << a << " " << b;
+#define os cout << " "
+#define all(v) v.begin(), v.end()
+#define mem(n, m) memset(n, m, sizeof(n))
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define sii set<int>
+#define sll set<long long>
+#define vii vector<int>
+#define vll vector<long long>
+#define mll map<long long, long long>
+#define lob lower_bound
+#define upb upper_bound
+#define ret return 0
+#define present(s, x) (s.find(x) != s.end())
+#define cpresent(s, x) (find(all(s), x) != s.end())
+
+#define boost ios_base::sync_with_stdio(0)
+#define MOD 1000000007
+#define EPSILON 1e-9
+#define PI 3.14159265358979323846
+#define inf 999999999999999999
+#define siz 100001
+#define SIZ 1000001
+#define SIZE 200001
+
+#define pqb priority_queue<int>
+#define pqs priority_queue<int, vi, greater<int>>
+#define setbits(x) __builtin_popcountll(x)
+#define zrobits(x) __builtin_ctzll(x)
+
+void solve() {
+#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#endif
+  ll t = 0;
+  cin >> t;
+  while (t--) {
+    ll n = 0;
+    cin >> n;
+    ll profit = 0;
+    while (n--) {
+      ll s, p, v;
+      cin >> s >> p >> v;
+      profit = max(profit, (p / (s + 1)) * v);
+    }
+    cout << profit;
+    on;
+  }
+}
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  solve();
+
+  return 0;
+}
