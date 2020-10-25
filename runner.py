136ae7303de7ae74ab5430d07e9c2d08191d83a9
Anik Das <anikdas0811@gmail.com>
2020-10-25 14:45:57 +0530

    - training sheet problems

--- runner.py
+++ runner.py
@@ -8,12 +8,13 @@ filepath = " ".join(file)
 
 if filepath[-3:].lower() == "cpp":
     print("$ Building ")
-    os.system("g++ "+filepath+" -o "+filepath[:-3]+"out")
+    os.system("g++ -std=c++17 "+filepath+" -o "+filepath[:-3]+"out")
     print("$ Executing ")
     os.system(filepath[:-3]+"out")
+
 elif filepath[-2:].lower() == "py":
     print("$ Running ")
     os.system("python3 "+filepath)
 
 else:
-    print("$ Languages other than cpp or py not accounted for, sadly")
+    print("$ Use good languages man")
